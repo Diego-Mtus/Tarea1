6d5d3b35207035cd8d98a9e56c8d5d756557d847
Diego-Mtus <dmatus2023@udec.cl>
2025-04-22 14:18:44 -0400

    Implementación de deposito genérico para las monedas y productos

--- src/Deposito.java
+++ src/Deposito.java
@@ -1,2 +1,23 @@
-public class Deposito {
+import java.util.ArrayList;
+
+public class Deposito<T>{
+
+    private T t;
+    private ArrayList<T> arrayList;
+
+    public Deposito(){
+        arrayList = new ArrayList<T>();
+    }
+
+    public void add(T t){
+        arrayList.add(t);
+    }
+
+    public T get(){
+        if(!arrayList.isEmpty()){
+            return arrayList.removeFirst();
+        } else{
+            return null;
+        }
+    }
 }
