6cd0f5a2ed0503537cc3fb908ce4573c8d97e1c7
Diego-Mtus <dmatus2023@udec.cl>
2025-04-22 13:56:17 -0400

    Implementación de la clase abstracta de Moneda junto a sus métodos

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,2 +1,21 @@
-public class Moneda {
+abstract public class Moneda implements Comparable<Moneda>{
+
+    public Moneda(){};
+
+    public Moneda getSerie(){
+        return this;
+    }
+
+    public abstract int getValor();
+
+    @Override
+    public int compareTo(Moneda m) {
+        if(this.getValor() > m.getValor()){
+            return 1;
+        } else if (this.getValor() == m.getValor()){
+            return 0;
+        } else {
+            return -1;
+        }
+    }
 }
