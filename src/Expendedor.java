96adcf635283ca8515bbab6d92176f7ce9e48f5f
DoggoMedic <josegonzalezag379@gmail.com>
2025-05-01 18:49:34 -0400

    Implementacion de comprar producto y gestion de excepciones

--- src/Expendedor.java
+++ src/Expendedor.java
@@ -21,6 +21,70 @@ public class Expendedor {
             depositoSnickers.add(new Snickers(400+i));
             depositoSuper8.add(new Super8(500+i));
         }
+
+    }
+
+    public Producto comprarProducto(Moneda m, ProductosEnum eleccion) throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException{
+        if(m == null){
+            throw new PagoIncorrectoException();
+        }
+        if(m.getValor() < eleccion.getPrecio()){
+            throw new PagoInsuficienteException();
+        } else {
+            Producto auxiliar;
+            switch (eleccion){
+                case COCACOLA -> {
+                    auxiliar = depositoCocaCola.get();
+                    if(auxiliar != null){
+                        procesarMonedas(ProductosEnum.COCACOLA.getPrecio(), depositoMonedas, m);
+                        return auxiliar;
+                    }
+                }
+                case FANTA -> {
+                    auxiliar = depositoFanta.get();
+                    if(auxiliar != null){
+                        procesarMonedas(ProductosEnum.FANTA.getPrecio(), depositoMonedas, m);
+                        return auxiliar;
+                    }
+                }
+                case SPRITE -> {
+                    auxiliar = depositoSprite.get();
+                    if(auxiliar != null){
+                        procesarMonedas(ProductosEnum.SPRITE.getPrecio(), depositoMonedas, m);
+                        return auxiliar;
+                    }
+                }
+                case SUPER8 -> {
+                    auxiliar = depositoSuper8.get();
+                    if(auxiliar != null){
+                        procesarMonedas(ProductosEnum.SUPER8.getPrecio(), depositoMonedas, m);
+                        return auxiliar;
+                    }
+                }
+                case SNICKERS -> {
+                    auxiliar = depositoSnickers.get();
+                    if(auxiliar != null){
+                        procesarMonedas(ProductosEnum.SNICKERS.getPrecio(), depositoMonedas, m);
+                        return auxiliar;
+                    }
+                }
+            }
+
+
+            throw new NoHayProductoException();
+        }
+    }
+
+    private void procesarMonedas(int precioEnum, Deposito<Moneda> depositoMoneda, Moneda m){
+        int auxPrecio = m.getValor() - precioEnum;
+        while (auxPrecio > 0) {
+            depositoMoneda.add(new Moneda100());
+            auxPrecio -= 100;
+        }
+    }
+
+    public Moneda getVuelto(){
+        return this.depositoMonedas.get();
     }
 
 }
