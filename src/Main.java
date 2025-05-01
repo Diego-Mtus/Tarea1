c7692728d9d57d6b405f24a3e4001e8bda833ddd
DoggoMedic <josegonzalezag379@gmail.com>
2025-05-01 18:54:38 -0400

    Realizacion de pruebas de expendedor y comprador

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
 
         Deposito<Moneda> depositoMonedas = new Deposito<>();
         depositoMonedas.add(new Moneda100());
@@ -11,9 +13,7 @@ public class Main {
         depositoMonedas.add(new Moneda100());
         depositoMonedas.add(new Moneda1000());
 
-        // ProductosEnum eleccion = ProductosEnum.COCACOLA;
-        Expendedor expendedor = new Expendedor(30);
-
+        System.out.println("\nPruebas del funcionamiento de deposito de Monedas con método sort() junto a  método toString():\n");
 
         // Prueba de print de ArrayList sin y con sort
         ArrayList<Moneda> ordenado = depositoMonedas.getArrayList();
@@ -22,18 +22,95 @@ public class Main {
             System.out.println(i.toString());
         }
 
-
-        System.out.println("\nValores de las monedas en arraylist ordenados:");
         ordenado.sort(Moneda::compareTo);
+
+        System.out.println("\nValores de las monedas en arraylist ordenados usando sort():");
         for(Moneda i : ordenado){
             System.out.println(i.toString());
         }
 
-        System.out.println("\nPrueba de uso de método getArrayList() para demostrar que arraylist original no se modifica:");
+        System.out.println("\nPrueba de usar nuevamente getArrayList para mostrar que orden original no se modifica, sino que se copia:");
         ArrayList<Moneda> pruebaDeCopiaNoReferencia = depositoMonedas.getArrayList();
         for(Moneda i : pruebaDeCopiaNoReferencia){
             System.out.println(i.toString());
         }
+
+        Expendedor expendedor = new Expendedor(1);
+        System.out.println("\nPruebas del expendedor de forma independiente.\n");
+
+        System.out.println("Prueba 1: Compra normal");
+        try {
+            Producto pruebaProducto = expendedor.comprarProducto(new Moneda1000(), ProductosEnum.COCACOLA);
+            System.out.println(pruebaProducto + " con número de serie " + pruebaProducto.getSerie());
+        } catch (PagoIncorrectoException | NoHayProductoException | PagoInsuficienteException e ) {
+            System.out.println("Error" + e);
+        }
+
+        System.out.println("\nPrueba 2: Compra con moneda null");
+        try {
+            Producto pruebaProducto = expendedor.comprarProducto(null, ProductosEnum.COCACOLA);
+            System.out.println(pruebaProducto);
+        } catch (PagoIncorrectoException | NoHayProductoException | PagoInsuficienteException e ) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("\nPrueba 3: Compra con plata insuficiente");
+        try {
+            Producto pruebaProducto = expendedor.comprarProducto(new Moneda100(), ProductosEnum.COCACOLA);
+            System.out.println(pruebaProducto);
+        } catch (PagoIncorrectoException | NoHayProductoException | PagoInsuficienteException e ) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("\nPrueba 4: Compra sin stock");
+        try {
+            Producto pruebaProducto = expendedor.comprarProducto(new Moneda1000(), ProductosEnum.COCACOLA);
+            System.out.println(pruebaProducto);
+        } catch (PagoIncorrectoException | NoHayProductoException | PagoInsuficienteException e ) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("\nPruebas de comprador");
+
+        Expendedor nuevo = new Expendedor(3);
+
+        System.out.println("\nComprador intenta pagar con moneda null:");
+        Comprador compradorSinDinero = new Comprador(null, 1,nuevo);
+
+        System.out.println("\nComprador intenta pagar con poco dinero:");
+        Comprador compradorPobre = new Comprador(new Moneda100(), 1,nuevo);
+        System.out.println("Vuelto: $" + compradorPobre.cuantoVuelto());
+
+        System.out.println("\nComprador puede pagar lo que quiere: ");
+        Comprador compradorExitoso = new Comprador(new Moneda1000(), 2, nuevo);
+        System.out.println("Vuelto: $" + compradorExitoso.cuantoVuelto());
+        System.out.println(compradorExitoso.queCompro());
+
+
+        System.out.println("\nPrueba de comprador que comprará todos los productos de un expendedor + 1 (Es decir, eventualmente pagará sin stock)");
+        int cantidadParaLoop = 3;
+        Expendedor expLoop = new Expendedor(cantidadParaLoop);
+
+        for(int i = 0; i <= cantidadParaLoop * ProductosEnum.values().length; i++){
+            Comprador compLoop = new Comprador(new Moneda1000(), (i % ProductosEnum.values().length) + 1, expLoop);
+            System.out.println(compLoop.queCompro());
+            System.out.println("Vuelto: $" + compLoop.cuantoVuelto() + "\n");
+
+        }
+
+
+        System.out.println("Prueba de cambiar el precio de un producto, se comprará el producto 2 veces y el cambió se verá reflejado en el vuelto.\n");
+
+        Expendedor expCambioPrecios = new Expendedor(2);
+        Comprador cambio1 = new Comprador(new Moneda1000(), ProductosEnum.SNICKERS.getIndice(), expCambioPrecios);
+        System.out.println(cambio1.queCompro());
+        System.out.println("Vuelto: $" + cambio1.cuantoVuelto() + "\n");
+
+        ProductosEnum.SNICKERS.setPrecio(900);
+        Comprador cambio2 = new Comprador(new Moneda1000(), ProductosEnum.SNICKERS.getIndice(), expCambioPrecios);
+        System.out.println(cambio1.queCompro());
+        System.out.println("Vuelto: $" + cambio2.cuantoVuelto() + "\n");
+
 
     }
 }
